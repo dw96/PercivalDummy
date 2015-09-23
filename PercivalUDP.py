7459bdbfc997da244ccfc69921eeb4440ab3f03b
Christian Angelsen <dw96@users.noreply.github.com>
2015-09-23 10:48:20 +0100

    Implemented select(), threading not needed - tested should work..

--- PercivalUDP.py
+++ PercivalUDP.py
@@ -4,99 +4,83 @@
 '''
 
 import socket, time, sys, numpy as np
-import threading
+from select import select
 from datetime import datetime
 
+def read_udp(sock, f, bFirstWrite, UDP_IP, UDP_PORT):
+
+    data, address = sock.recvfrom(8000)
+    # Only print "header" info once to file
+    if bFirstWrite:
+        print >> f, "________________________________________"
+        print >> f, ("From: %s targeting: %s:%d\n" % ( address, UDP_IP, UDP_PORT))
+        headerInfo(f)
+        print >> f, "________________________________________\n"
+    
+    packetCounter = 0
+    # Extract Percival header from packet's payload
+    npData = np.fromstring(data[:23], dtype='uint8')
+    # Only want first & last packets of each subframe
+    if (npData[7] < 10) or (npData[7] > 245):
+        if npData[7] == 0:
+            print >> f, ("\npktNum:%i (unchanged)" % packetCounter)
+        # 22 byte header of each packet
+        for index in range(22):
+            if index % 8 == 0:
+                print >> f, "  ",
+            print >> f, ("%02X" % npData[index]),
+        #
+        print >> f, ""
+    
 def PercivalUDP(address):
-    UDP_IP   = address    # "10.1.0.1"
-    UDP_PORTS= (8000, 8001)
-    
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.settimeout(60)
-        
-    # Chop off the rest in a separate function, 1 thread for each port
-    #   Whichever threading.Thread(..) Comes first, is executed first..
-    lr = threading.Thread(target=listenForData(sock, UDP_IP, UDP_PORTS[1]))
-    ld = threading.Thread(target=listenForData(sock, UDP_IP, UDP_PORTS[0]))
-    ld.start()
-    lr.start() 
-    
-    #TODO: Need to consider select that select to avoid one  sock.recv() blocking the other thread...?
-    
-def listenForData(sock, UDP_IP, UDP_PORT):
-    
-    print "Instance (attempting) listening to: [%s:%d]" % (UDP_IP, UDP_PORT)
-    try:
-        sock.bind((UDP_IP, UDP_PORT))
-    except Exception as e:
-        print UDP_PORT, "PRT Socket [%s:%d] Setup error:" % (UDP_IP, UDP_PORT), e
 
-    # Open and close dummy file
-    fileName = "/tmp/no_such.file"
-    f = open(fileName, "w")
-    f.close()
+    UDP_PORTS = (8000, 8001)
 
-    print UDP_PORT, "Listening to %s:%d." % (UDP_IP, UDP_PORT)
-    time.sleep(0.1)
-    (subframe, data, packetCounter) = (0, 0, 0)
-    listFiles = []
+    dateString = datetime.now().strftime("%Y%m%d-%H%M%S")
+    fileName00 = "/tmp/Percy__%s--%s.txt" % (str(UDP_PORTS[0]), dateString)
+    f00 = open(fileName00, "w")
+    
+    # Repeat for second file handle
+    fileName01 = "/tmp/Percy__%s--%s.txt" % (str(UDP_PORTS[1]), dateString)
+    f01 = open(fileName01, "w")
+    
+    # create udp socket - port 8000
+    udp00 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    udp00.bind((address,UDP_PORTS[0]))
+
+    # create udp socket - port 8001
+    udp01 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    udp01.bind((address,UDP_PORTS[1]))
+
+    input = [udp00,udp01]
+
+    (bFirstWrite0, bFirstWrite1)  = (True, True)
     try:
+        print "Listening for incoming data.."
         while True:
-            # Receive UDP data, convert into unsigned 8-bit integer
-            packetCounter += 1
-            try:
-                data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-                if packetCounter == 1:
-                    print UDP_PORT, "Data received!\n"
-                    sys.stdout.flush()
-                # Data received, open file handle if shut
-                if f.closed:
-                    dateString = datetime.now().strftime("%Y%m%d-%H%M%S")
-                    fileName = "/tmp/Percy_headers_Port_%s-%s.txt" % (str(UDP_PORT), dateString)
-                    f = open(fileName, "w")
-                    print >> f, "________________________________________"
-                    print >> f, ("Target: %s:%d\n" % (UDP_IP, UDP_PORT))
-                    headerInfo(f)
-                    print >> f, "________________________________________\n"
-                    try:
-                        sock.settimeout(3)
-                    except Exception as e:
-                        print UDP_PORT, "Couldn't reduce socket timeout: %s" % e
-                        #print "PRT Socket [%s:%d] Setup error:" % (UDP_IP, UDP_PORT), e
-                        return
+            inputready,outputready,exceptready = select(input,[],[])
 
-            except socket.timeout as e: # e = "timed out"
-                # Close file handle (until data available again)
-                if not f.closed:
-                    listFiles.append(fileName)
-                    f.close()
-                    print UDP_PORT, "Data received; Awaiting next data.."
-                    sys.stdout.flush()
+            for s in inputready:
+                if s == udp00:
+#                     print "udp00"
+#                     time.sleep(1)
+                    sender = read_udp(s, f00, bFirstWrite0, address, UDP_PORTS[0])
+                    bFirstWrite0 = False
+                elif s == udp01:
+#                     print "udp01"
+#                     time.sleep(1)
+                    sender = read_udp(s, f01, bFirstWrite1, address, UDP_PORTS[1])
+                    bFirstWrite1 = False
                 else:
-                    print UDP_PORT, ".",
-                    sys.stdout.flush()
-
-                print UDP_PORT, "No data after waiting 10 seconds; Closing down.."
-                for index in listFiles:
-                    print UDP_PORT, "(Saved file %s) " % index
-                break
-
-            npData = np.fromstring(data[:23], dtype='uint8')
-            # Only display first & last packets of each subframe
-            if (npData[7] < 10) or (npData[7] > 245):
-                if npData[7] == 0:
-                    print >> f, ("\npktNum:%i" % packetCounter)
-                # Display 22 byte header of each packet
-                for index in range(22):
-                    if index % 8 == 0:
-                        print >> f, "  ",
-                    print >> f, ("%02X" % npData[index]),
-                #
-                print >> f, ""
-    except Exception as e:
-        print UDP_PORT, "Unsuspected error:", e
-
-
+                    print "unknown socket:", s
+    except KeyboardInterrupt:
+        print " *** User interrupted programme"
+        if not f00.closed:
+            f00.close()
+        if not f01.closed:
+            f01.close()
+    print "Created filenames:\n '%s'\n '%s'" % (fileName00, fileName01)
+            
 def headerInfo(fHandle):
     ''' Display internal boundaries within the UDP header '''
     print >> fHandle, "   PktType [1 Byte]"
